07396e7504e190afac7a33d1dafbcca9260b934d
Kushal Pandya <kushalpandya@outlook.com>
2016-01-25 14:44:52 +0530

    Flashybox Plugin source.

---
+++ src/jquery.flashybox.js
@@ -0,0 +1,10 @@
+/**
+ * Flashybox v0.1.0
+ * A jQuery Plugin to Show image collage as flashy boxes.
+ *
+ *
+ * Author: Kushal Pandya <kushalspandya@gmail.com> (https://doublslash.com)
+ * Date: 25 January, 2015
+ *
+ * Flashybox Plugin.
+ */
